2a90605b9874834a1a8c7a33affeb16bdc2b0f08
Arth Kamboj <arth23153015@akgec.ac.in>
2026-04-27 18:51:49 +0530

    added functions to implement compiler logic

---
+++ src/compiler.cpp
@@ -0,0 +1,13 @@
+#include "compiler.h"
+#include <iostream>
+#include <stdexcept>
+
+using namespace std;
+
+void Compiler::compile(const LispVal& ast){
+
+}
+
+void Compiler::disassemble(){
+    
+}
