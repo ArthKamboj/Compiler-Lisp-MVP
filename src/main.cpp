6f578b6bd7fd78feab72bc6089b0e7f6651fc252
Arth Kamboj <arth23153015@akgec.ac.in>
2026-04-28 20:26:28 +0530

    Fully implemented expression compilation and output generation

--- src/main.cpp
+++ src/main.cpp
@@ -38,25 +38,15 @@ void execute(const string& code, shared_ptr<Environment> env) {
 
 int main() {
 
-    string code = "(+ (* 2 3 4) (- (* 10 5) (/ 100 2) (* 3 3)) (/ (+ (* 7 8) (- 50 20) (* 2 2 2)) 5) (* (+ 1 2 3 4 5) (- 20 10 5) (/ 90 3)) (- (+ (* 6 6) (/ 144 12) (* 2 5)) (* 3 7)) (/ (* (+ 8 2) (- 15 5) (* 3 3)) (+ 5 5)) (* (- 100 50 25) (+ 4 4 4) (/ 60 2)))";
-    cout << "Input code: " << code << "\n";
-
     try{
 
-        vector<string> token_vec = tokenize(code);
-        list<string> tokens(token_vec.begin(), token_vec.end());
-        
-        LispVal ast = parse(tokens);
+        cout << "----Micro-Lisp Compiler and VM----" << "\n\n";
 
-        cout << "AST parseed successfully: ";
-        print_lisp_val(ast);
-        cout << "\n\n";
+        auto global_env = make_shared<Environment>();
 
-        cout << "-----------Bytecode Compilation-----------" << endl;
-
-        Compiler compiler;
-        compiler.compile(ast);
-        compiler.disassemble();
+        execute("(define radius 5)", global_env);
+        execute("(define pi 3.14159)", global_env);
+        execute("(* pi (* radius radius))", global_env);
 
     }
     catch(const exception e){
