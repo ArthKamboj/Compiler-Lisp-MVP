9a4ec945a47770ad2246d5a08722ffc271aaca7e
Arth Kamboj <arth23153015@akgec.ac.in>
2026-05-06 16:56:38 +0530

    upgraded lexer to tokenize multiple words accordingly

--- src/lexer.cpp
+++ src/lexer.cpp
@@ -1,27 +1,60 @@
 #include "lexer.h"
 #include <sstream>
+#include <cctype>
 
 using namespace std;
 
-vector<string> tokenize(const string& input) {
-    string padded_input;
-    
-    for (char c : input) {
-        if (c == '(') {
-            padded_input += " ( ";
-        } else if (c == ')') {
-            padded_input += " ) ";
-        } else {
-            padded_input += c;
+#include "lexer.h"
+#include <cctype>
+
+using namespace std;
+
+vector<string> tokenize(const string& code) {
+    vector<string> tokens;
+    string current_token = "";
+    bool in_string = false;
+
+    for (size_t i = 0; i < code.length(); ++i) {
+        char c = code[i];
+
+        if (in_string) {
+            current_token += c;
+            if (c == '"') {
+                tokens.push_back(current_token);
+                current_token = "";
+                in_string = false;
+            }
+        } 
+        else {
+            if (c == '"') {
+                if (!current_token.empty()) {
+                    tokens.push_back(current_token);
+                    current_token = "";
+                }
+                current_token += c;
+                in_string = true;
+            } 
+            else if (c == '(' || c == ')') {
+                if (!current_token.empty()) {
+                    tokens.push_back(current_token);
+                    current_token = "";
+                }
+                tokens.push_back(string(1, c));
+            } 
+            else if (isspace(c)) {
+                if (!current_token.empty()) {
+                    tokens.push_back(current_token);
+                    current_token = "";
+                }
+            } 
+            else {
+                current_token += c;
+            }
         }
     }
-
-    vector<string> tokens;
-    stringstream ss(padded_input);
-    string token;
     
-    while (ss >> token) {
-        tokens.push_back(token);
+    if (!current_token.empty()) {
+        tokens.push_back(current_token);
     }
     
     return tokens;
