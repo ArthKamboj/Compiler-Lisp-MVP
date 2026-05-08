1511eeb3e46b8b24597cdb96315efed1f675fbcf
Arth Kamboj <arth23153015@akgec.ac.in>
2026-05-08 19:49:47 +0530

    implemnted raw pointers and marker in en

--- src/environment.h
+++ src/environment.h
@@ -3,30 +3,61 @@
 #include <string>
 #include <unordered_map>
 #include <stdexcept>
-#include <memory>
+#include "gc.h"
 
 using namespace std;
 
-class Environment{
+inline void mark_lisp_val(const LispVal& val);
 
-    private:
-        unordered_map<string, LispVal> symbol_table;
-        shared_ptr<Environment> outer;
+class Environment : public GCObject {
 
     public:
-        Environment() : outer(nullptr) {}
-        Environment(shared_ptr<Environment> outer_env) : outer(outer_env) {}
-        void set(const string& symbol, const LispVal& val){
+        unordered_map<string, LispVal> symbol_table;
+        Environment* parent;
+        
+        Environment(Environment* parent = nullptr) : parent(parent) {}
+
+        void set(const string& symbol, const LispVal& val) {
             symbol_table.insert_or_assign(symbol, val);
         }
-        LispVal get(const string& symbol){
+
+        LispVal get(const string& symbol) {
             auto it = symbol_table.find(symbol);
             if(it != symbol_table.end()){
                 return it->second;
             }
-            if(outer){
-                return outer->get(symbol);
+            if(parent){
+                return parent->get(symbol);
             }
             throw runtime_error("Unbound Symbol: "+symbol);
         }
+
+        void mark() {
+            if (marked) return;
+            marked = true;
+
+            if (parent) parent->mark();
+
+            for(auto& pair : symbol_table) {
+                mark_lisp_val(pair.second);
+            }
+        }
 };
+
+inline void mark_lisp_val (const LispVal& val) {
+    if(holds_alternative<GCList*>(val.value)) {
+        GCList* list = get<GCList*>(val.value);
+        if (list && !list->marked) {
+            list->marked = true;
+            for (const auto& item : list->items) {
+                mark_lisp_val(item);
+            }
+        }
+    }
+    else if (holds_alternative<LispFunction>(val.value)) {
+            LispFunction func = get<LispFunction>(val.value);
+        if (func.env) {
+            func.env->mark();
+        }
+    }
+}
