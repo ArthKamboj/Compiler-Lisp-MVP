3e5ebe88e760db3cfe22f7f94a0a858a501af7fe
Arth Kamboj <arth23153015@akgec.ac.in>
2026-05-08 19:02:47 +0530

    implemented sweeping function of GC

---
+++ src/gc.cpp
@@ -0,0 +1,23 @@
+#include "gc.h"
+
+using namespace std;
+
+void GarbageCollector::sweep() {
+    vector<GCObject*> surviving_objects;
+    int deleted_count = 0;
+
+    for(GCObject* obj : heap) {
+        if (obj->marked) {
+            obj->marked = false;
+            surviving_objects.push_back(obj);
+        }
+        else {
+            delete obj;
+            deleted_count++;
+        }
+    }
+
+    heap = surviving_objects;
+}
+
+cout << "\n[GC] Swept " << deleted_count << " objects. Heap size: " << heap.size() << endl;
